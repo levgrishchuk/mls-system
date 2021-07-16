de8755783469e8fc3fa472f06e0b13a23aac2d6d
Rafsun Masud <rafsun82@my.yorku.ca>
2021-07-16 13:00:53 -0400

    relocated Storage class

---
+++ src/main/java/mls/property/structure/exterior/Storage.java
@@ -0,0 +1,10 @@
+/*
+package mls.property.structure.exterior;
+
+import mls.property.structure.exterior.*;
+
+public class Storage extends Exterior {
+    // fields
+    private int lockerCount;
+}
+*/
