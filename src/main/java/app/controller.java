c937a2f57ea540b4934f9e854a83c28aa3a279b5
moonwane <68343698+moonwane@users.noreply.github.com>
2021-07-17 17:45:28 -0400

    testing gson implementation

--- src/main/java/app/controller.java
+++ src/main/java/app/controller.java
@@ -1,14 +1,26 @@
 package app;
-import java.io.File;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-import org.json.JSONArray;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import mls.Brokerage;
 
 /**
  * Controls CRUD functionality
  */
 public class controller {
+    // Reading demo
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream inputstream = new FileInputStream("E:\\personal_programming_projects\\York schoolwork\\S2021\\EECS2030\\A3\\src\\main\\resources\\data.json");
+        JsonReader reader = new JsonReader(new InputStreamReader(inputstream));
 
-
-
+        Gson g = new Gson();
+        Brokerage brokerage = g.fromJson(reader, Brokerage.class);
+        System.out.println(brokerage);
+    }
 }
+
+
