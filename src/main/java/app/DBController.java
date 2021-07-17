efcaa01f1abf84c43a758bd721a6c9a87693b6e1
Rafsun Masud <rafsun82@my.yorku.ca>
2021-07-17 17:52:26 -0400

    added singleton code on DBController

--- src/main/java/app/DBController.java
+++ src/main/java/app/DBController.java
@@ -1,4 +1,21 @@
 package app;
 
+/** This class controls database */
 public class DBController {
+    private DBController instance;
+    private final String jsonPath = "src/main/resources/data.json";
+
+
+    /** No instantiation allowed */
+    private DBController() {};
+
+    /**
+     * Returns the singleton instance.
+     */
+    public DBController getInstance() {
+        if (instance == null) {
+            instance = new DBController();
+        }
+        return instance;
+    }
 }
